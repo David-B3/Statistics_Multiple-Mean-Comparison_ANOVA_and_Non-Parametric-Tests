c3ce0fd3fe9e870f57e2cdd68d5454821060b6bd
David-B3 <113585472+David-B3@users.noreply.github.com>
2022-09-16 12:05:20 +0200

    Update MultipleMeanComparison.R

--- MultipleMeanComparison.R
+++ MultipleMeanComparison.R
@@ -15,7 +15,7 @@
 # STUDY-CASE: 
 # 22 subjects played a football simulated game. Before the match ("PreMatch"), 24h, 48h and 72h after, they performed different type of force 
 # measurements (IMVC, NAV and DB100). After the match, subjects were divided in 2 groups experiencing 25-min sessions of either cold water immersion (CWI, n = 11) or 
-# hot water immersion (HWI, n = 11) during this recovery period. We want to know if the immersion temperature have an impact on the force recovery. To do so, we will 
+# hot water immersion (HWI, n = 11). We want to know if the immersion temperature have an impact on the force recovery. To do so, we will 
 # perform an Multiple Mean Comparison analysis. 
 
 # SOURCES :
